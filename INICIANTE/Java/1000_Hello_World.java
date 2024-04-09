55a98456cdc997b254b55322b846fb74b3926141
Vitoria Helen <vitoriahellen3004@gmail.com>
2024-04-09 18:43:42 -0300

    resolucao hello world - exercicio 1000
    - Exercicio 1 do modulo iniante do beecrowd
    - Hello World! nas linguagens C, Java, JS e Python

---
+++ INICIANTE/Java/1000_Hello_World.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+ 
+ 
+public class 1000_Hello_World { 
+    public static void main(String[] args) throws IOException {
+ 
+        System.out.println("Hello World!");
+ 
+    }
+ 
+}
