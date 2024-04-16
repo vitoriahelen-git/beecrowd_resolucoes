612da1fc6e7ea0a354ef11391058d36998cdfcd6
Vitoria Helen <vitoriahellen3004@gmail.com>
2024-04-15 21:13:04 -0300

    Resolve exercicio 6 (1005 - Media 1)
    - Resolucao do exercicio 6 do modulo iniciante do beecrowd
    - Media 1 = (peso1 + peso2) = 11
                 Media = (A * peso1 + B * peso2)/11
    - Linguagens: C, JAVA, Java Script e Python

---
+++ INICIANTE/Java/Media1_1005.java
@@ -0,0 +1,33 @@
+// Leia 2 valores de ponto flutuante de dupla precisão A e B, que correspondem 
+// a 2 notas de um aluno. A seguir, calcule a média do aluno, sabendo que a nota 
+// A tem peso 3.5 e a nota B tem peso 7.5 (A soma dos pesos portanto é 11). 
+// Assuma que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
+
+// Entrada
+// O arquivo de entrada contém 2 valores com uma casa decimal cada um.
+
+// Saída
+// Imprima a mensagem "MEDIA" e a média do aluno conforme exemplo abaixo, 
+// com 5 dígitos após o ponto decimal e com um espaço em branco antes e depois da igualdade. 
+// Utilize variáveis de dupla precisão (double) e como todos os problemas, não esqueça de imprimir 
+// o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+
+import java.io.IOException;
+import java.util.Scanner; 
+
+public class Media1_1005 {
+    public static void main(String[] args) throws IOException{
+        try(Scanner lerTeclado = new Scanner(System.in)){
+            double A, B, MEDIA; 
+            A = lerTeclado.nextDouble();
+            B = lerTeclado.nextDouble(); 
+            A = A * 3.5; 
+            B = B * 7.5; 
+            MEDIA = (A + B)/11; 
+
+            System.out.printf("MEDIA = %.5f\n", MEDIA);
+
+        }
+    }
+    
+}
