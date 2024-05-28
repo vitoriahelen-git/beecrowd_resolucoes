a7eb5170db0fe74988ca9be515afaabd943aeeb4
Vitoria Helen <vitoriahellen3004@gmail.com>
2024-05-27 21:32:18 -0300

    Resolve exercicio 1008 - Salario (beecrowd)
    - Resolucao do exercicio 1008 do modulo iniciante do beecrowd
    - Salario = QuantidadeHoras * ValorHora
    - Linguagens: C, JAVA, Java Script e Python

---
+++ INICIANTE/C/1008_salario.c
@@ -0,0 +1,28 @@
+// Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas, 
+// o valor que recebe por hora e calcula o salário desse funcionário. A seguir, mostre o número 
+// e o salário do funcionário, com duas casas decimais.
+
+// Entrada
+// O arquivo de entrada contém 2 números inteiros e 1 número com duas casas decimais, 
+// representando o número, quantidade de horas trabalhadas e o valor que o funcionário 
+// recebe por hora trabalhada, respectivamente.
+
+// Saída
+// Imprima o número e o salário do funcionário, conforme exemplo fornecido, com um espaço em 
+// branco antes e depois da igualdade. No caso do salário, também deve haver um espaço em branco após o $.
+
+#include <stdio.h>
+
+int main(){
+    int numero, Qthoras; 
+    float valorHora, salario; 
+
+    scanf("%d %d %f", &numero, &Qthoras, &valorHora);
+
+    salario = Qthoras*valorHora; 
+
+    printf("NUMBER = %d\n", numero);  
+    printf("SALARY = U$ %.2f\n", salario);  
+
+    return 0; 
+}
---
+++ INICIANTE/Java/Salario_1008.java
@@ -0,0 +1,35 @@
+// Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas, 
+// o valor que recebe por hora e calcula o salário desse funcionário. A seguir, mostre o número 
+// e o salário do funcionário, com duas casas decimais.
+
+// Entrada
+// O arquivo de entrada contém 2 números inteiros e 1 número com duas casas decimais, 
+// representando o número, quantidade de horas trabalhadas e o valor que o funcionário 
+// recebe por hora trabalhada, respectivamente.
+
+// Saída
+// Imprima o número e o salário do funcionário, conforme exemplo fornecido, com um espaço em 
+// branco antes e depois da igualdade. No caso do salário, também deve haver um espaço em branco após o $.
+
+import java.io.IOException;
+import java.util.Scanner; 
+
+public class Salario_1008 {
+    public static void main(String[] args) throws IOException{
+        try(Scanner lerTeclado = new Scanner(System.in)){
+            int numero, QtHoras; 
+            float valorHora, salario; 
+
+            numero = lerTeclado.nextInt(); 
+            QtHoras = lerTeclado.nextInt(); 
+            valorHora = lerTeclado.nextFloat(); 
+
+            salario = QtHoras * valorHora; 
+
+            System.out.println("NUMBER = "+ numero);
+            System.out.printf("SALARY = U$ %.2f", salario);
+
+        }
+    }
+    
+}
